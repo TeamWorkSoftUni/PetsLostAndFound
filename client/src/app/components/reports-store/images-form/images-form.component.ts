130fa3436714674912d17e7fe3f38cacbcf3a88b
Georgiew3979 <d.georgiew@mail.bg>
2017-12-19 20:27:01 +0200

    Add ng-cloudinary for images

--- client/src/app/components/reports-store/images-form/images-form.component.ts
+++ client/src/app/components/reports-store/images-form/images-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { CloudinaryOptions, CloudinaryUploader } from 'ng2-cloudinary'
+
 
 @Component({
   selector: 'app-images-form',
@@ -6,8 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./images-form.component.css']
 })
 export class ImagesFormComponent implements OnInit {
+  imagesId: [string];
+  @Output() uploadedImagesId = new EventEmitter()
+  
+  uploader: CloudinaryUploader = new CloudinaryUploader(
+      new CloudinaryOptions({ cloudName: 'dqoigfeno', uploadPreset: 'ntgsijh9' })
+  );
 
-  constructor() { }
+  constructor(){
+      //Override onSuccessItem to retrieve the imageId
+      this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any): any => {
+          let res: any = JSON.parse(response);
+          if(!this.imagesId){
+            this.imagesId = ['']
+            this.imagesId[0] = res.public_id;
+          } else {
+            this.imagesId.push(res.public_id);
+          }
+          
+          
+          return { item, response, status, headers };
+      };
+  }
+
+  upload() {
+      this.uploader.uploadAll();
+      this.uploadedImagesId.emit(this.imagesId)
+  }
 
   ngOnInit() {
   }
--- client/src/app/components/reports-store/reports.module.ts
+++ client/src/app/components/reports-store/reports.module.ts
@@ -4,17 +4,22 @@ import { AppRoutingModule } from '../../app-routing.module';
 import { LocationFormComponent } from './location-form/location-form.component';
 import { ReportsComponent } from './reports/reports.component';
 import { ReportFormComponent } from './report-form/report-form.component';
+import { ImagesFormComponent } from './images-form/images-form.component';
+import { PetFormComponent } from './pet-form/pet-form.component';
 
 // Modules
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AgmCoreModule } from '@agm/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2CloudinaryModule } from 'ng2-cloudinary';
+import { FileUploadModule } from 'ng2-file-upload';
+
 
 // Services
 import { ReportService } from '../../services/report.service';
-import { ImagesFormComponent } from './images-form/images-form.component';
-import { PetFormComponent } from './pet-form/pet-form.component';
+
 
 
 
@@ -35,7 +40,10 @@ import { PetFormComponent } from './pet-form/pet-form.component';
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBEFONV9Qm5seghvVJomqOmmXY5fHqxcDY',
       libraries: ["places"]
-    })
+    }),
+    BrowserModule,
+    Ng2CloudinaryModule,
+    FileUploadModule
   ],
   exports: [ 
     ReportsComponent,
