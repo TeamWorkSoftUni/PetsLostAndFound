30815eea60a41432b539a0ea71964c85292480f3
Dimitar Nikolov <zeitgeistdimi@gmail.com>
2018-01-09 21:48:24 +0200

    Deleting '<<<<HEAD' commends

--- PetsLostAndFound.Models/PetsLostAndFound.Web/Controllers/AccountController.cs
+++ PetsLostAndFound.Models/PetsLostAndFound.Web/Controllers/AccountController.cs
@@ -13,7 +13,6 @@ namespace PetsLostAndFound.Web.Controllers
 
 
   [Authorize]
-<<<<<<< HEAD
   public class AccountController : BaseController
   {
     private readonly UserManager<User> userManager;
@@ -32,105 +31,7 @@ namespace PetsLostAndFound.Web.Controllers
       this.emailSender = emailSender;
       this.logger = logger;
     }
-=======
-  [Route("[controller]/[action]")]
-  public class AccountController : Controller
-    {
-        private readonly UserManager<User> _userManager;
-        private readonly SignInManager<User> _signInManager;
-        private readonly IEmailSender _emailSender;
-        private readonly ILogger _logger;
 
-        public AccountController(
-            UserManager<User> userManager,
-            SignInManager<User> signInManager,
-            IEmailSender emailSender,
-            ILogger<AccountController> logger)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _emailSender = emailSender;
-            _logger = logger;
-        }
-
-        [TempData]
-        public string ErrorMessage { get; set; }
-
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IActionResult> Login(string returnUrl = null)
-        {
-            // Clear the existing external cookie to ensure a clean login process
-            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
-
-            ViewData["ReturnUrl"] = returnUrl;
-            return Ok();
-        }
-
-        [HttpPost]
-        [AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login([FromBody]LoginViewModel model, string returnUrl = null)
-        {
-            ViewData["ReturnUrl"] = returnUrl;
-            if (ModelState.IsValid)
-            {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User logged in.");
-                    return RedirectToLocal(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToAction(nameof(LoginWith2fa), new { returnUrl, model.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToAction(nameof(Lockout));
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return View(model);
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return Ok(model);
-        }
-
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IActionResult> LoginWith2fa(bool rememberMe, string returnUrl = null)
-        {
-            // Ensure the user has gone through the username & password screen first
-            var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
-
-            if (user == null)
-            {
-                throw new ApplicationException($"Unable to load two-factor authentication user.");
-            }
-
-            var model = new LoginWith2faViewModel { RememberMe = rememberMe };
-            ViewData["ReturnUrl"] = returnUrl;
-
-            return View(model);
-        }
-
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> LoginWith2fa(LoginWith2faViewModel model, bool rememberMe, string returnUrl = null)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
->>>>>>> e56d7b23ebf54997a9494fc880acbacc9cd5faff
 
     [TempData]
     public string ErrorMessage { get; set; }
@@ -179,10 +80,9 @@ namespace PetsLostAndFound.Web.Controllers
       return Ok();
     }
 
-<<<<<<< HEAD
     [HttpPost]
     [AllowAnonymous]
-    [ValidateAntiForgeryToken]
+    //[ValidateAntiForgeryToken]
     public async Task<IActionResult> Register([FromBody] PostRegisterRequestModel model, string returnUrl = null)
     {
       ViewData["ReturnUrl"] = returnUrl;
@@ -193,29 +93,6 @@ namespace PetsLostAndFound.Web.Controllers
         if (result.Succeeded)
         {
           this.logger.LogInformation("User created a new account with password.");
-=======
-        [HttpPost]
-        [AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register([FromForm]RegisterViewModel model, string returnUrl = null)
-        {
-            ViewData["ReturnUrl"] = returnUrl;
-            if (ModelState.IsValid)
-            {
-                var user = new User
-                {
-                    UserName = model.Email,
-                    Email = model.Email,
-                    PhoneNumber = model.telephoneNumber,
-                    TownName = model.locationName,
-                    Image = model.userImagesId
-                };
-                var result = await _userManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User created a new account with password.");
->>>>>>> e56d7b23ebf54997a9494fc880acbacc9cd5faff
-
           var code = await this.userManager.GenerateEmailConfirmationTokenAsync(user);
 
           await this.signInManager.SignInAsync(user, isPersistent: false);
@@ -225,96 +102,9 @@ namespace PetsLostAndFound.Web.Controllers
         }
         AddErrors(result);
       }
-
-<<<<<<< HEAD
       // If we got this far, something failed, redisplay form
       return Ok(model);
     }
-=======
-        [HttpPost]
-        //[ValidateAntiForgeryToken]
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
-            return Ok();
-        }
-
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public IActionResult ExternalLogin(string provider, string returnUrl = null)
-        {
-            // Request a redirect to the external login provider.
-            var redirectUrl = Url.Action(nameof(ExternalLoginCallback), "Account", new { returnUrl });
-            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
-            return Challenge(properties, provider);
-        }
-
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IActionResult> ExternalLoginCallback(string returnUrl = null, string remoteError = null)
-        {
-            if (remoteError != null)
-            {
-                ErrorMessage = $"Error from external provider: {remoteError}";
-                return RedirectToAction(nameof(Login));
-            }
-            var info = await _signInManager.GetExternalLoginInfoAsync();
-            if (info == null)
-            {
-                return RedirectToAction(nameof(Login));
-            }
-
-            // Sign in the user with this external login provider if the user already has a login.
-            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
-            if (result.Succeeded)
-            {
-                _logger.LogInformation("User logged in with {Name} provider.", info.LoginProvider);
-                return RedirectToLocal(returnUrl);
-            }
-            if (result.IsLockedOut)
-            {
-                return RedirectToAction(nameof(Lockout));
-            }
-            else
-            {
-                // If the user does not have an account, then ask the user to create an account.
-                ViewData["ReturnUrl"] = returnUrl;
-                ViewData["LoginProvider"] = info.LoginProvider;
-                var email = info.Principal.FindFirstValue(ClaimTypes.Email);
-                return View("ExternalLogin", new ExternalLoginViewModel { Email = email });
-            }
-        }
-
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ExternalLoginConfirmation(ExternalLoginViewModel model, string returnUrl = null)
-        {
-            if (ModelState.IsValid)
-            {
-                // Get the information about the user from the external login provider
-                var info = await _signInManager.GetExternalLoginInfoAsync();
-                if (info == null)
-                {
-                    throw new ApplicationException("Error loading external login information during confirmation.");
-                }
-                var user = new User { UserName = model.Email, Email = model.Email };
-                var result = await _userManager.CreateAsync(user);
-                if (result.Succeeded)
-                {
-                    result = await _userManager.AddLoginAsync(user, info);
-                    if (result.Succeeded)
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        _logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
-                        return RedirectToLocal(returnUrl);
-                    }
-                }
-                AddErrors(result);
-            }
->>>>>>> e56d7b23ebf54997a9494fc880acbacc9cd5faff
 
     [HttpPost]
     [ValidateAntiForgeryToken]
@@ -402,15 +192,6 @@ namespace PetsLostAndFound.Web.Controllers
     {
       return View();
     }
-
-<<<<<<< HEAD
-=======
-        [HttpGet]
-        public IActionResult AccessDenied()
-        {
-            return Ok();
-        }
->>>>>>> e56d7b23ebf54997a9494fc880acbacc9cd5faff
 
     [HttpGet]
     public IActionResult AccessDenied()
--- PetsLostAndFound.Models/PetsLostAndFound.Web/Startup.cs
+++ PetsLostAndFound.Models/PetsLostAndFound.Web/Startup.cs
@@ -8,14 +8,10 @@ namespace PetsLostAndFound.Web
   using Microsoft.EntityFrameworkCore;
   using Microsoft.Extensions.Configuration;
   using Microsoft.Extensions.DependencyInjection;
-<<<<<<< HEAD
   using PetsLostAndFound.Db;
   using PetsLostAndFound.Db.Models;
   using PetsLostAndFound.Web.Infrastructure.Extencions;
-=======
-  using PetsLostAndFound.Data;
-  using PetsLostAndFound.Data.Models;
->>>>>>> e56d7b23ebf54997a9494fc880acbacc9cd5faff
+
   using Services;
 
   public class Startup
@@ -24,8 +20,6 @@ namespace PetsLostAndFound.Web
     {
       Configuration = configuration;
     }
-<<<<<<< HEAD
-
     public IConfiguration Configuration { get; }
     
     public void ConfigureServices(IServiceCollection services)
@@ -33,20 +27,39 @@ namespace PetsLostAndFound.Web
       services.AddDbContext<PetsDbContext>(options =>
           options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-      services.AddIdentity<User, IdentityRole>()
+      services.AddIdentity<User, IdentityRole>(options =>
+          {
+              options.Password.RequireDigit = false;
+              options.Password.RequireNonAlphanumeric = false;
+              options.Password.RequireLowercase = false;
+              options.Password.RequireUppercase = false;
+          })
           .AddEntityFrameworkStores<PetsDbContext>()
           .AddDefaultTokenProviders();
       
       services.AddTransient<IEmailSender, EmailSender>();
 
-      services.AddCors();
+            //services.AddCors();
 
-      services.AddMvc();
-      services.Configure<MvcOptions>(options =>
-      {
-        options.Filters.Add(new CorsAuthorizationFilterFactory("http://localhost:4200"));
-      });
-    }
+            //services.AddMvc();
+            //services.Configure<MvcOptions>(options =>
+            //{
+            //  options.Filters.Add(new CorsAuthorizationFilterFactory("http://localhost:4200"));
+        //});
+
+        services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder.WithOrigins("http://localhost:4200"));
+            });
+        
+        services.AddMvc();
+
+        services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new CorsAuthorizationFilterFactory("AllowSpecificOrigin"));
+            });
+        }
 
 
     public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -65,92 +78,6 @@ namespace PetsLostAndFound.Web
       app.UseAuthentication();
 
       app.UseMvc();
-=======
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<PetsDbContext>(options =>
-          options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-      services.AddIdentity<User, IdentityRole>(options =>
-        {
-          options.Password.RequireDigit = false;
-          options.Password.RequireNonAlphanumeric = false;
-          options.Password.RequireLowercase = false;
-          options.Password.RequireUppercase = false;
-        })
-          .AddEntityFrameworkStores<PetsDbContext>()
-          .AddDefaultTokenProviders();
-
-      // Add application services.
-      services.AddTransient<IEmailSender, EmailSender>();
-
-      //services.AddCors();
-
-      //services.AddMvc();
-      //services.Configure<MvcOptions>(options =>
-      //{
-      //  options.Filters.Add(new CorsAuthorizationFilterFactory("http://localhost:4200"));
-      //});
-
-      //services.AddMvc(options =>
-      //{
-      //  options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-      //});
-
-      services.AddCors(options =>
-            {
-              options.AddPolicy("AllowSpecificOrigin",
-              builder => builder.WithOrigins("http://localhost:4200"));
-            });
-
-      services.AddMvc();
-      services.Configure<MvcOptions>(options =>
-      {
-        options.Filters.Add(new CorsAuthorizationFilterFactory("AllowSpecificOrigin"));
-      });
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      //app.Use(async (context, next) =>
-      //{
-      //  await next();
-      //  if (context.Response.StatusCode == 404 &&
-      //      !Path.HasExtension(context.Request.Path.Value) &&
-      //      !context.Request.Path.Value.StartsWith("/api/"))
-      //  {
-      //    context.Request.Path = "/index.html";
-      //    await next();
-      //  }
-      //});
-
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseBrowserLink();
-        app.UseDatabaseErrorPage();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
-      }
-
-      app.UseStaticFiles();
-
-      app.UseAuthentication();
-
-      app.UseMvc(routes =>
-      {
-        routes.MapRoute(
-            name: "default",
-            template: "{controller=Home}/{action=Index}/{id?}");
-      });
->>>>>>> e56d7b23ebf54997a9494fc880acbacc9cd5faff
     }
   }
 }
